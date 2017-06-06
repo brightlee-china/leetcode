b8ae27e671e9eae92a95cfe6dc5753f88eece46d
brightlee <liliang@ios.ac.cn>
2017-06-06 20:51:03 +0800

    leetcode main

---
+++ leetcode-main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cctype>
+#include <string.h>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque> 
+#include <stack>
+
+using namespace std;
+
+int main() {
+    cout << 1 << endl;
+    return 0;
+}
